afa8603fb6781530fdf628aef417017d18c79b70
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-16 23:16:11 +0100

    refactor(core/Report): updated class to use refactored types

--- core/modules/Report/class.ts
+++ core/modules/Report/class.ts
@@ -10,6 +10,7 @@ export type ReportMember = {
 export class ServerReport {
     // Store the report at it's id key
     static activeReports: Map<string, ServerReport> = new Map();
+    static database = new DB();
 
     static newReport(
         reporter: ReportMember,
@@ -21,16 +22,18 @@ export class ServerReport {
 
         const report = new ServerReport({
             id,
-            reporter,
+            reporter_license: reporter.license,
+            reporter_name: reporter.name,
             subject,
             messages: [{
                 message: initialMessage,
-                author: reporter,
-                ts: date,
+                author_license: reporter.license,
+                author_name: reporter.name,
+                timestamp: date,
             }],
             status: ServerReportStatus.OPEN,
-            tsOpened: date,
-            tsLastAction: date,
+            ts_opened: date,
+            ts_lastaction: date,
         });
 
         this.activeReports.set(id, report);
@@ -55,84 +58,89 @@ export class ServerReport {
     }
 
     private id: ServerReportDto['id'];
-    private reporter: ServerReportDto['reporter'];
+    private reporter_license: ServerReportDto['reporter_license'];
+    private reporter_name: ServerReportDto['reporter_name'];
     private subject: ServerReportDto['subject'];
     private status: ServerReportDto['status'];
     private messages: ServerReportDto['messages'];
-    private tsOpened: ServerReportDto['tsOpened'];
-    private tsLastAction: ServerReportDto['tsLastAction'];
+    private ts_opened: ServerReportDto['ts_opened'];
+    private ts_lastaction: ServerReportDto['ts_lastaction'];
 
-    private database: DB;
     /**
      * Create a new report
      */
     constructor(reportDto: ServerReportDto) {
         this.id = reportDto.id;
-        this.reporter = reportDto.reporter;
+        this.reporter_license = reportDto.reporter_license;
+        this.reporter_name = reportDto.reporter_name;
         this.subject = reportDto.subject;
         this.status = reportDto.status;
         this.messages = reportDto.messages;
-        this.tsOpened = reportDto.tsOpened;
-        this.tsLastAction = reportDto.tsLastAction;
-
-        this.database = new DB();
+        this.ts_opened = reportDto.ts_opened;
+        this.ts_lastaction = reportDto.ts_lastaction;
 
         this.initialDBSave();
     }
 
     initialDBSave() {
-        const exists = this.database.query<{ '1': 1 }>('SELECT 1 FROM reports WHERE `id` = ?', [this.id]);
+        const exists = ServerReport.database.query<{ '1': 1 }>('SELECT 1 FROM reports WHERE `id` = ?', [this.id]);
 
         if (exists) return;
 
-        this.database.insert('reports', {
+        ServerReport.database.insert('reports', {
             id: this.id,
-            reporter_license: this.reporter.license,
-            reporter_name: this.reporter.name,
+            reporter_license: this.reporter_license,
+            reporter_name: this.reporter_name,
             subject: this.subject,
             status: this.status,
-            ts_opened: this.tsOpened,
-            ts_lastaction: this.tsLastAction
+            ts_opened: this.ts_opened,
+            ts_lastaction: this.ts_lastaction
         });
 
         for (const msg of this.messages) {
-            this.database.insert('reports_messages', {
+            ServerReport.database.insert('reports_messages', {
                 report_id: this.id,
-                author_license: msg.author.license,
-                author_name: msg.author.name,
+                author_license: msg.author_license,
+                author_name: msg.author_name,
                 message: msg.message,
-                timestamp: msg.ts
+                timestamp: msg.timestamp
             });
         }
     }
 
     newMessage(message: string, author: ReportMember, isAdmin: boolean = false) {
-        const ts = Date.now();
+        const timestamp = Date.now();
         
-        const msgObj = { message, author, ts };
-        this.messages.push(msgObj);
-        this.tsLastAction = ts;
+        this.messages.push({
+            message,
+            author_license: author.license,
+            author_name: author.name,
+            timestamp
+        });
+        this.ts_lastaction = timestamp;
 
-        this.database.insert('reports_messages', {
+        ServerReport.database.insert('reports_messages', {
             report: this.id,
             author_license: author.license,
             author_name: author.name,
             message: message,
-            timestamp: ts
+            timestamp
         });
 
         const sql = `UPDATE reports SET ts_lastaction = ? WHERE id = ?`;
-        this.database.query(sql, [ts, this.id]);
+        ServerReport.database.query(sql, [timestamp, this.id]);
 
         if (this.status === ServerReportStatus.OPEN && isAdmin) {
             this.status = ServerReportStatus.INPROGRESS;
 
             const sql = `UPDATE reports SET status = ? WHERE id = ?`;
-            this.database.query(sql, [this.status, this.id]);
+            ServerReport.database.query(sql, [this.status, this.id]);
         }
 
         txCore.webServer.webSocket.sendReportMessage(this.id, {
-            message, author, ts,
+            message, timestamp,
+            author_license: author.license,
+            author_name: author.name,
         });
     }
 
@@ -140,18 +148,19 @@ export class ServerReport {
         this.newMessage('Ticket has been closed', author);
 
         const sql = `UPDATE reports SET ts_lastaction = ? WHERE id = ?`;
-        this.database.query(sql, [this.id]);
+        ServerReport.database.query(sql, [this.id]);
     }
 
     getData(): ServerReportDto {
         return {
             id: this.id,
-            reporter: this.reporter,
+            reporter_license: this.reporter_license,
+            reporter_name: this.reporter_name,
             subject: this.subject,
             status: this.status,
             messages: this.messages,
-            tsOpened: this.tsOpened,
-            tsLastAction: this.tsLastAction,
+            ts_opened: this.ts_opened,
+            ts_lastaction: this.ts_lastaction,
         }
     }
 }
